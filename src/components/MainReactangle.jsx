65a4c72a3226c2ab1c21974901b197bdcc81afea
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-07-07 14:05:35 +0530

    money img added

--- src/components/MainReactangle.jsx
+++ src/components/MainReactangle.jsx
@@ -4,29 +4,51 @@ import ReferModal from './ReferModal';
 
 const MainReactangle = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  
+
 
   return (
     <div className="flex justify-center">
       {/* parent div */}
-    <div className="bg-blue-200  max-w-4xl shadow-lg rounded-3xl  flex justify-between my-10  ">
-      {/* child div 1 */}
-      <div className="w-1/3 mx-12 my-20" >
-    
-        <h2 className="text-5xl font-bold mb-2">Let's Learn <h1>& Earn</h1></h2>
-        <p className="text-lg ">Get a chance to win <p>up-to <span className='text-3xl font-bold text-blue-700'>Rs. 15,000</span></p> </p>
-        <button  onClick={() => setModalIsOpen(true)}
- className="bg-blue-700 mt-10 text-white px-7 py-2 rounded-lg">Refer Now</button>
-  <ReferModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} />
-
-      </div>
-      {/* child div 2 */}
-      <div className="w-1/2 mt-6 mx-3" >
-        <img  src="/image1.png" alt="Learn and Earn..." className="w-full h-full object-contain"
-         />
-         
+      <div className="relative bg-blue-200  max-w-4xl shadow-lg rounded-3xl  flex justify-between my-10 overflow-hidden ">
+        <img className="absolute w-32 -top-10 rotate-180" src="/money.png" />
+        <img
+          className="absolute w-32 -top-14 right-8 -rotate-60"
+          src="/money.png"
+        />
+        <img
+          className="absolute w-32 bottom-2 right-1/4 -rotate-180"
+          src="/money.png"
+        />
+        {/* child div 1 */}
+        <div className="w-1/3 mx-12 my-20">
+          <h2 className="text-5xl font-bold mb-2">
+            Let's Learn <h1>& Earn</h1>
+          </h2>
+          <p className="text-lg ">
+            Get a chance to win{" "}
+            <p>
+              up-to{" "}
+              <span className="text-3xl font-bold text-blue-700">
+                Rs. 15,000
+              </span>
+            </p>{" "}
+          </p>
+          <button onClick={() => setModalIsOpen(true)} className="bg-blue-700 mt-10 text-white px-7 py-2 rounded-lg">
+            Refer Now
+          </button>
+          <ReferModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} />
+        </div>
+        {/* child div 2 */}
+        <div className="w-1/2 mt-6 mx-3 z-100">
+          <img
+            src="/image1.png"
+            alt="Learn and Earn..."
+            className="w-full h-full object-contain z-100"
+          />
+        </div>
       </div>
     </div>
-  </div>
 
   )
 }
